a2e2ca2d64bbceced41d93b0966b501917665d99
afaraitsulaimon <afaraitsulaimon@gmail.com>
2022-05-05 16:53:36 +0100

    Done with the About us Page

--- abryd-app/src/App.js
+++ abryd-app/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import BookKeeping from './pages/BookKeeping';
 import CompanyReg from './pages/CompanyReg';
 import Procurement from './pages/Procurement';
+import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs'
+
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
         <Route exact path='/book-keeping' element={<BookKeeping />} />
         <Route exact path='/company-reg' element={<CompanyReg />} />
         <Route exact path='/procurement-services' element={<Procurement />} />
+        <Route exact path='/contact-us' element={<ContactUs />} />
+        <Route exact path='/about-us' element={<AboutUs />} />
+
 
         </Routes>
       </BrowserRouter>
